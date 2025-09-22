3cc4029cb7741bb7e88c28a360131754d00c9aa1
imPCamuriDev <pedrogabrielofc@hotmail.com>
2025-09-22 20:05:28 -0300

    CORS added

---
+++ src/main/java/com/pepo/notasapi/Config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.pepo.notasapi.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH") 
+                .allowedHeaders("*") 
+                .exposedHeaders("Authorization", "Content-Type")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
